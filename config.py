8154d9f79b6459f89ac7a98751e4ef27592a8c8f
Vitória Selista <anavselista@protonmail.com>
2023-11-09 22:19:38 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from requests.auth import HTTPBasicAuth
 
 # The base URL of your instance
-base_url = "https://{YOUR-INSTANCE-NAME}.atlassian.net/"
+base_url = "https://{YOUR-INSTANCE-NAME}.atlassian.net"
 
 # Set the authentication credentials using HTTP Basic Authentication
 credentials = HTTPBasicAuth("{YOUR-EMAIL}", "{API-TOKEN}")
